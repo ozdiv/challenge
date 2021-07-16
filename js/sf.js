0151813c10f5d3a014e2acc583ea17b1f2a3e1b3
ozdiv <serge@ozdiv.com>
2021-07-16 13:56:41 +1000

    sfed to sf

--- js/sf.js
+++ js/sf.js
@@ -1,29 +1,29 @@
 "use strict";
-var sfed = {};
+var sf = {};
 
-sfed.addStyle = function (sel, prop, val) {
-  sfed.styleElements(sfed.getElements(sel), prop, val);
+sf.addStyle = function (sel, prop, val) {
+  sf.styleElements(sf.getElements(sel), prop, val);
 };
-sfed.styleElements = function (elements, prop, val) {
+sf.styleElements = function (elements, prop, val) {
   var i, l = elements.length;
   for (i = 0; i < l; i++) {    
-    sfed.styleElement(elements[i], prop, val);
+    sf.styleElement(elements[i], prop, val);
   }
 };
-sfed.styleElement = function (element, prop, val) {
+sf.styleElement = function (element, prop, val) {
   element.style.setProperty(prop, val);
 };
 
-sfed.addClass = function (sel, name) {
-  sfed.addClassElements(sfed.getElements(sel), name);
+sf.addClass = function (sel, name) {
+  sf.addClassElements(sf.getElements(sel), name);
 };
-sfed.addClassElements = function (elements, name) {
+sf.addClassElements = function (elements, name) {
   var i, l = elements.length;
   for (i = 0; i < l; i++) {
-    sfed.addClassElement(elements[i], name);
+    sf.addClassElement(elements[i], name);
   }
 };
-sfed.addClassElement = function (element, name) {
+sf.addClassElement = function (element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -31,16 +31,16 @@ sfed.addClassElement = function (element, name) {
     if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
   }
 };
-sfed.removeClass = function (sel, name) {
-  sfed.removeClassElements(sfed.getElements(sel), name);
+sf.removeClass = function (sel, name) {
+  sf.removeClassElements(sf.getElements(sel), name);
 };
-sfed.removeClassElements = function (elements, name) {
+sf.removeClassElements = function (elements, name) {
   var i, l = elements.length, arr1, arr2, j;
   for (i = 0; i < l; i++) {
-    sfed.removeClassElement(elements[i], name);
+    sf.removeClassElement(elements[i], name);
   }
 };
-sfed.removeClassElement = function (element, name) {
+sf.removeClassElement = function (element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -51,16 +51,16 @@ sfed.removeClassElement = function (element, name) {
   }
   element.className = arr1.join(" ");
 };
-sfed.getElements = function (id) {
+sf.getElements = function (id) {
   if (typeof id == "object") {
     return [id];
   } else {
     return document.querySelectorAll(id);
   }
 };
-sfed.filterHTML = function(id, sel, filter) {
+sf.filterHTML = function(id, sel, filter) {
   var a, b, c, i, ii, iii, hit;
-  a = sfed.getElements(id);
+  a = sf.getElements(id);
   for (i = 0; i < a.length; i++) {
     b = a[i].querySelectorAll(sel);
     for (ii = 0; ii < b.length; ii++) {
